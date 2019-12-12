ca8115d42c69f3fd01fb5487cf30f4274e28d063
Mikko Liikanen <mikkol@spotify.com>
2019-12-12 23:48:37 +0100

    Day 5 - intcode machine evolves

---
+++ day5/src/main/kotlin/intcode/IntcodeComputer.kt
@@ -0,0 +1,82 @@
+package intcode
+
+import intcode.ops.*
+import java.lang.Exception
+import java.lang.UnsupportedOperationException
+
+private lateinit var readInput: () -> Int
+private lateinit var writeOuput: (Int) -> Unit
+
+class IntcodeComputer( val memory: Array<Int> ) {
+    constructor(vararg params: Int) : this(params.toTypedArray())
+
+    fun run(input: () -> Int = { throw UnsupportedOperationException("No input defined") },
+            output: (Int) -> Unit = { println(it) },
+            debug: Boolean = false) {
+        readInput = input
+        writeOuput = output
+        run(memory.clone(), debug = debug)
+    }
+
+    private fun run(mem: Array<Int>,
+                    opCtrInit: Int = 0,
+                    debug: Boolean = false) {
+        var ctr = opCtrInit;
+        try {
+            while (true) {
+                val opcode = Opcode(mem[ctr])
+                val operation = opcode.toOperation(mem, ctr)
+                if (debug) println("$ctr ::: ${runtimeDump(mem, ctr)} ::: $operation")
+                val nextInstructionFunction = operation.eval()
+                ctr = nextInstructionFunction(ctr)
+            }
+            //run(mem, operation.eval()(ctr))
+        } catch (e: Halt) {
+            println("HALT")
+        } catch (e: Exception) {
+            println("Crash at op $ctr\nMemory content around op:\n${crashdump(mem, ctr)} \n$e");
+        }
+    }
+
+    private fun crashdump(mem: Array<Int>, ctr: Int): String {
+        return (Math.max(0, ctr - 5) until Math.min(mem.size, ctr + 8))
+                .map { "  $it :: ${mem[it]}" }
+                .joinToString(separator = "\n")
+
+    }
+
+    private fun runtimeDump(mem: Array<Int>, ctr: Int): String {
+        return (ctr until Math.min(mem.size, ctr + 6))
+                .map { "${mem[it]}" }
+                .joinToString(separator = ",")
+
+    }
+
+}
+
+
+
+
+
+class InputToMemory(private val param: Param) : Operation {
+    override fun eval(): (Int) -> Int {
+        param.write(readInput())
+        return { it + 2 }
+    }
+
+    override fun toString(): String {
+        return "ITM(param=$param)"
+    }
+}
+
+class MemoryToOutput(private val param: Param) : Operation {
+    override fun eval(): (Int) -> Int {
+        writeOuput(param.read())
+        return { it + 2 }
+    }
+
+    override fun toString(): String {
+        return "MTO(param=$param)"
+    }
+}
+
---
+++ day5/src/main/kotlin/intcode/ops/Multiply.kt
@@ -0,0 +1,18 @@
+package intcode.ops
+
+import intcode.Param
+
+class Multiply(private val a: Param,
+               private val b: Param,
+               private val result: Param) : Operation {
+    override fun eval(): (Int) -> Int {
+        result.write(a.read() * b.read())
+        return { it + 4 }
+    }
+
+    override fun toString(): String {
+        return "MUL(a=$a, b=$b, result=$result)"
+    }
+
+
+}
